f7ad58a8b2d5587af979e0158b07e1e3a9e4bc3f
Edward Noramirian <e.noramirian@gmail.com>
2021-02-02 12:12:27 +0400

    main

--- src/containers/PokemonPageContainer/index.jsx
+++ src/containers/PokemonPageContainer/index.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
 import "./pokemon_page_container.scss";
-import { PokemonTable } from "../../components";
 
 import { PageNotFoundContainer } from "..";
+
+import { PokemonTable } from "../../components";
+import { ReactComponent as RightArrow } from "../../assets/right_arrow.svg";
 
 const PokemonPageContainer = () => {
   const pathname = useLocation().pathname;
@@ -24,6 +27,40 @@ const PokemonPageContainer = () => {
     pokemon?.name.charAt(0).toUpperCase() +
     pokemon?.name.slice(1) +
     " | Pokédex";
+
+  const PrevBtn = () => {
+    const prevPokemonName = pokemons[pokemonData?.id - 2]?.name;
+
+    return pokemonData.id !== 1 ? (
+      <Link to={"/" + prevPokemonName}>
+        <div className="prev_next_btn flex_center" role="button">
+          <RightArrow className="flip" />
+        </div>
+      </Link>
+    ) : (
+      <div
+        className="prev_next_btn"
+        style={{ backgroundColor: "transparent" }}
+      ></div>
+    );
+  };
+
+  const NextBtn = () => {
+    const nextPokemonName = pokemons[pokemonData?.id]?.name;
+
+    return pokemonData.id !== pokemons.length ? (
+      <Link to={"/" + nextPokemonName}>
+        <div className="prev_next_btn flex_center" role="button">
+          <RightArrow />
+        </div>
+      </Link>
+    ) : (
+      <div
+        className="prev_next_btn"
+        style={{ backgroundColor: "transparent" }}
+      ></div>
+    );
+  };
 
   useEffect(() => {
     if (!pokemon) return;
@@ -44,8 +81,22 @@ const PokemonPageContainer = () => {
       </Helmet>
       <div className="pokemon_page_container">
         <div className="container">
+          <div className="top_btns">
+            <PrevBtn />
+            <Link to="/">
+              <div className="home_btn" role="button">
+                <RightArrow className="flip" />
+                <p>Home</p>
+              </div>
+            </Link>
+            <NextBtn />
+          </div>
           <div>
-            <h2 className="pokemon_name">{pokemon.name}</h2>
+            <div className="title_and_btns flex_center">
+              <PrevBtn />
+              <h2 className="pokemon_name">{pokemon.name}</h2>
+              <NextBtn />
+            </div>
             <p className="pokemon_id">#{pokemonId}</p>
           </div>
           <div className="content">
